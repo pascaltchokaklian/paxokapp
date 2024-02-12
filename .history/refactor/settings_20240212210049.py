0a43d27c42a28e3a9092090bda05832e16c0a6a2
pascaltchokaklian <pascaltchokaklian@outlook.com>
2024-02-12 21:05:08 +0100

    ALLOWED_HOSTS

--- .history/refactor/settings_20240212210049.py
+++ .history/refactor/settings_20240212210049.py
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure--d34*jma0c$uwulyce2&h^2ypc+iw1mz8l^fgvvl0*pi)#1*4y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS=["localhost", "127.0.0.1","centcols7-245eaaaf.b4a.run"]
-
+#ALLOWED_HOSTS=["localhost", "127.0.0.1","centcols7-245eaaaf.b4a.run"]
+ALLOWED_HOSTS=["centcols7-245eaaaf.b4a.run"]
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Application definition
