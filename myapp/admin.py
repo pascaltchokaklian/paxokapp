39b4e674d6cae46513697dd5eba316226543dd1f
pascaltchokaklian <pascaltchokaklian@outlook.com>
2026-03-09 15:54:37 +0100

    Ajout de l'affichage et du filtrage des entraîneurs dans les listes d'activités

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Activity
 
 # Register your models here.
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = (
+        'act_id', 'act_name', 'act_start_date', 'act_dist',
+        'act_den', 'act_normal_power', 'act_type', 'act_trainer'
+    )
+    list_filter = ('act_type', 'act_trainer')
+    search_fields = ('act_name',)
